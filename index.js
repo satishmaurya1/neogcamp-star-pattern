78e820666f02fb08f1ed61d5f2a8bb160afbf5fb
Satish Maurya <satishmaurya15216@gmail.com>
2020-12-04 05:58:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+ 
+ let level="";
+ 
+ 
+ for(let i=5; i>=1; i--){
+            for(let j=1; j<=i; j++){
+
+
+              
+                level =level+"*";
+                
+            
+            }
+            console.log(level)
+            level="";
+            
+        }
